8a3c96934faf39b42957d8606fbf34544066cb8e
NKrein <nikokrein@gmail.com>
2024-06-19 21:10:53 -0300

    Clase 2: Componentes, props y estados

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,43 @@
+import React, { useState } from 'react'
 import './App.css'
 
-const App = (props) => {
+function Titulo(props) { // <-- Recibimos las props por parametro
 
-  let nombre = ""
+  let nombre = props.nombre // <-- Utilizamos la propiedad "nombre" que contiene el objeto "props"
   if (nombre === "") {
     nombre = "usuario"
-  }
-
-  function saludar () {
-    alert(`Bienvenido ${nombre}`)
   }
 
   return (
     <>
       <h1 className='dark-mode'>
-        ¡Hola {nombre}!
+        {/* Mostramos el nombre que recibimos por props, que almacenamos en la variable "nombre" */}
+        ¡Hola {nombre}! Estoy siendo renderizado desde JS
       </h1>
-      <button onClick={saludar}>Quiero que me saluden.</button>
+      {/* Mostramos el valor del contador, que recibimos por props bajo el nombre de "numero" */}
+      <p>Bienvenido a nuestra APP, que se renderizó {props.numero} veces.</p>
+    </>
+  )
+}
+
+
+const App = (props) => {
+  // [ valor del estado , funcion para manejar estado ] = useState(valorInicial)
+  const [contador, setContador] = useState(0)
+
+  const sumar = () => {
+    // Ejecutamos la funcion para cambiar le valor del estado
+    setContador(contador + 1) // <-- Le pasamos el nuevo valor entre los parentesis
+  }
+
+  return (
+    <>
+      {/* Ejecutamos el componente "Titulo" y le pasamos las propiedades como si fueran atributos  */}
+      <Titulo nombre='Martin' numero={contador}/>
+      {/* Mostramos el valor actual del contador, para eso usamos las llaves y el nombre del estado */}
+      <h2>{contador}</h2>
+      {/* Ejecutamos la funcion cuando el usuario hace click en el botón */}
+      <button onClick={sumar}>Sumar</button>
     </>
   )
 }
